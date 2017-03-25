53708c3e16a27df4a93e0d7a1f60908c665ca84b
TUnit17 <thuang177@gmail.com>
2017-03-24 17:44:23 -0700

    Broken silver

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 import MapKit
 
-class MapViewController :UIViewController
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate
+//class MapViewController :UIViewController
 {
 
     // Variable
@@ -18,6 +19,13 @@ class MapViewController :UIViewController
     
     // If a view controller is asked for its view and its view is nil, then the loadView() method is called
     override func loadView() {
+        
+        // Silver Challenge
+        mapView.delegate = self
+        
+        locationManager = CLLocationManager()
+        
+        
         // Create a map view
         mapView = MKMapView()
         
@@ -144,7 +152,53 @@ class MapViewController :UIViewController
     }
 
 
-    // programmatice view test 
+    
+    /**
+     Silver challenge
+     
+     What does this method do? 
+    **/
+    func initLocalizationButton(_ anyView: UIView!){
+        let localizationButton = UIButton.init(type: .system)
+        localizationButton.setTitle("Localization", for: .normal)
+        localizationButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(localizationButton)
+        
+        //Constraints
+        
+        let topConstraint = localizationButton.topAnchor.constraint(equalTo:anyView
+            .topAnchor, constant: 32 )
+        let leadingConstraint = localizationButton.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor)
+        let trailingConstraint = localizationButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        localizationButton.addTarget(self, action: #selector(MapViewController.showLocalization(sender:)), for: .touchUpInside)
+        
+        
+    } // end 1
+    
+    
+    // ?
+    func showLocalization(sender: UIButton!){
+        locationManager.requestWhenInUseAuthorization()//se agrega permiso en info.plist
+        mapView.showsUserLocation = true //fire up the method mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation)
+        
+        
+    }
+    
+    // ?
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        //This is a method from MKMapViewDelegate, fires up when the user`s location changes
+        let zoomedInCurrentLocation = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 500, 500)
+        mapView.setRegion(zoomedInCurrentLocation, animated: true)
+        
+        
+        //mapView.userTrackingMode = .follow
+        
+    }
     
     
 } //end class
